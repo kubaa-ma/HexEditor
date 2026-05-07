87005d2af0ae6caca255a2d7c2808e6c663176f0
Kander <jansa063@gmail.com>
2026-05-07 18:11:08 +0200

    Refactor SDL initialization

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,101 @@
 #include <SDL3/SDL_opengl.h>
 #endif
 
+#ifdef __EMSCRIPTEN__
+#include "../libs/emscripten/emscripten_mainloop_stub.h"
+#endif
 #include <iostream>
 
+int init_all(SDL_Window*& window, SDL_GLContext& gl_context){
+    if(!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)){
+        std::cout << "Error in SDL_Init(): " << SDL_GetError() << std::endl;
+        return 1; 
+    }
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+    // Create window with graphics context
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
+    SDL_WindowFlags window_flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN | SDL_WINDOW_HIGH_PIXEL_DENSITY;
+    window = SDL_CreateWindow("HexaEdit", (int)(1280 * main_scale), (int)(800 * main_scale), window_flags);
+    if (window == nullptr){
+        std::cout << "Error: SDL_CreateWindow(): %s\n" <<  SDL_GetError() << std::endl;
+        return 1;
+    }
+    gl_context = SDL_GL_CreateContext(window);
+    if (gl_context == nullptr){
+        std::cout << "Error: SDL_GL_CreateContext(): %s\n" << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_GL_MakeCurrent(window, gl_context);
+    SDL_GL_SetSwapInterval(1);
+    SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+    SDL_ShowWindow(window);
+
+    
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup scaling
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.ScaleAllSizes(main_scale);        // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
+    style.FontScaleDpi = main_scale;        // Set initial font scale. (in docking branch: using io.ConfigDpiScaleFonts=true automatically overrides this for every window depending on the current monitor)
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL3_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    return 0;
+}
+
+void loop_run(SDL_Window* window){
+    bool done = false;
+
+    while(!done){
+        SDL_Event event;
+        while(SDL_PollEvent(&event)){
+            ImGui_ImplSDL3_ProcessEvent(&event);
+            if(event.type == SDL_EVENT_QUIT)
+                done = true;
+            if(event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(window))
+                done = true;
+        }
+
+    }
+}
+
+void shut_down(SDL_Window* window, SDL_GLContext gl_context){
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DestroyContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
 int main(int, char**){
+    SDL_Window* window = nullptr;
+    SDL_GLContext gl_context = nullptr;
+    init_all(window, gl_context);
+    loop_run(window);
+    shut_down(window, gl_context);
 
     return 0;
 }
