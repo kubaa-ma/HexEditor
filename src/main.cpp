3b4e75a6f2dcd76edbdc49066efee94161a6c6f8
Kander <jansa063@gmail.com>
2026-05-07 17:36:55 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_opengl3.h"
+#include <SDL3/SDL.h>
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <SDL3/SDL_opengles2.h>
+#else
+#include <SDL3/SDL_opengl.h>
+#endif
+
+#include <iostream>
+
+int main(int, char**){
+
+    return 0;
+}
